e2d520a6fa5cc27bfdb30f0c435461baae9b31a9
Saade <saade@outlook.com.br>
2025-03-13 20:38:33 -0300

    refactor: simplify team invitation email by removing account creation instructions

--- resources/views/emails/team-invitation.blade.php
+++ resources/views/emails/team-invitation.blade.php
@@ -1,19 +1,7 @@
 @component('mail::message')
 {{ __('You have been invited to join the :team team!', ['team' => $invitation->team->name]) }}
 
-@if (Filament\Facades\Filament::getCurrentPanel()->hasRegistration())
-{{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation:') }}
-
-@component('mail::button', ['url' => Filament\Facades\Filament::getCurrentPanel()->getRegistrationUrl()])
-{{ __('Create Account') }}
-@endcomponent
-
-{{ __('If you already have an account, you may accept this invitation by clicking the button below:') }}
-
-@else
 {{ __('You may accept this invitation by clicking the button below:') }}
-@endif
-
 
 @component('mail::button', ['url' => $acceptUrl])
 {{ __('Accept Invitation') }}
