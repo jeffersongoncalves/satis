506de8a8872f4ba2e9e51298e1dd02e9d522b718
Saade <saade@outlook.com.br>
2025-03-18 23:25:44 -0300

    feat: add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\FilamentServiceProvider::class,
     App\Providers\FilamentPanelProvider::class,
+    App\Providers\FilamentServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
