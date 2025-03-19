ee3561a5f01151f224ca950cdc17c922b22d3d5f
Saade <saade@outlook.com.br>
2025-03-18 21:15:11 -0300

    fix: create a membership to its own team upon user creation

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,14 +2,21 @@
 
 namespace App\Observers;
 
+use App\Models\Team;
 use App\Models\User;
 
 class UserObserver
 {
     public function created(User $user): void
     {
-        $user->ownedTeams()->create([
-            'name' => explode(' ', $user->name, 2)[0]."'s Team",
-        ]);
+        tap(
+            Team::forceCreate([
+                'user_id' => $user->id,
+                'name' => explode(' ', $user->name, 2)[0]."'s Team",
+            ]),
+            function (Team $team) use ($user) {
+                $user->teams()->attach($team);
+            }
+        );
     }
 }
